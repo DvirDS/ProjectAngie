c7fcd57839b8e384da968090e63f769e7e79671d
May Seter <maysetter@gmail.com>
2026-08-19 23:02:41 +0300

    fixed respawn

--- Assets/Scripts/Core/RespawnPointProvider.cs
+++ Assets/Scripts/Core/RespawnPointProvider.cs
@@ -7,6 +7,7 @@ public class RespawnPointProvider : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
+        if (respawnPoint == null) return;
         RespawnManager.I.RespawnPoint = respawnPoint;
     }
 }
